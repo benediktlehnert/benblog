6cc326a57a29fc2e16dea0793cd4c81d7dd68a3b
Benedikt Lehnert <benedikt@6wunderkinder.com>
2012-12-30 23:03:03 +0100

    New blog posts and styles

--- site/templates/blog.php
+++ site/templates/blog.php
@@ -17,11 +17,11 @@
   <nav class="pagination">
 
     <?php if($articles->pagination()->hasNextPage()): ?>
-    <a class="next" href="<?php echo $articles->pagination()->nextPageURL() ?>">&lsaquo; newer posts</a>
+    <a class="next" href="<?php echo $articles->pagination()->nextPageURL() ?>">&lsaquo; older posts</a>
     <?php endif ?>
-
     <?php if($articles->pagination()->hasPrevPage()): ?>
-    <a class="prev" href="<?php echo $articles->pagination()->prevPageURL() ?>">older posts &rsaquo;</a>
+    <span style="font-style: normal; margin: 0 7px 0 10px;">·</span>
+    <a class="prev" href="<?php echo $articles->pagination()->prevPageURL() ?>">newer posts &rsaquo;</a>
     <?php endif ?>
 
   </nav>
