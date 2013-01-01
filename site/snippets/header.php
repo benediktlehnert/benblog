95190051574be4ab54f3c64f280172fb88a130c2
Benedikt Lehnert <benedikt@6wunderkinder.com>
2013-01-01 15:38:07 +0100

    added feed

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -10,10 +10,12 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="assets/favicon.ico" rel="shortcut icon">
+  	<link href="assets/favicon.ico" rel="shortcut icon">
 	<link href="assets/apple-touch-icon.png" rel="apple-touch-icon">
 
-  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,900' rel='stylesheet' type='text/css'>
+	<link rel="alternate" type="application/rss+xml" href="<?php echo url('blog/feed') ?>" title="<?php echo html($pages->find('blog/feed')->title()) ?>" />
+
+  	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,900' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Crimson+Text:400,400italic' rel='stylesheet' type='text/css'>
 
   <?php echo css('assets/styles/styles.css') ?>
--- site/templates/blogarticle.php
+++ site/templates/blogarticle.php
@@ -10,6 +10,7 @@
 			<a href="https://twitter.com/share" class="twitter-share-button" data-text="I just read a blog post:" data-via="blehnert">Tweet</a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 		</div>
+		<div class="tweet-post">Liked what you just read? Why not <a href="https://twitter.com/intent/tweet?source=webclient&amp;text=<?php echo rawurlencode("I just read a blog post: " . $page->title() . " (via @blehnert) " . $page->tinyurl()); ?>" target="blank" title="Tweet this">tweet</a> about it or <a href="http://yourfeedlink.com" title="Subscribe to my feed" rel="alternate" type="application/rss+xml">subscribe</a> to get updates on new posts!</div>
 	</div>
 	
     <a href="<?php echo $articles->pagination()->nextPageURL() ?>">Next</a>
