79e10b082128b588b21911fa880234d9dd70052c
Benedikt Lehnert <benedikt@6wunderkinder.com>
2013-01-02 00:01:19 +0100

    changed about page

--- site/templates/about.php
+++ site/templates/about.php
@@ -3,11 +3,23 @@
 <?php snippet('submenu') ?> -->
 
 <section class="content">
-	<img src="assets/images/ben_profile.jpg" alt="" />
-  <article>
-    <h1><?php echo html($page->title()) ?></h1>
-    <?php echo kirbytext($page->text()) ?>
-  </article>
+	<div style="text-align: center;">
+		<img class="about" src="../assets/images/ben_profile.jpg" alt="" />
+	</div>
+  	<article>
+    	<h1><?php echo html($page->title()) ?></h1>
+    	<?php echo kirbytext($page->text()) ?>
+  	</article>
+  	<div class="contact">
+  		<ul>
+  			<!-- <li><a href="http://www.twitter.com/blehnert">&#62217;</a></li>
+  			<li><a href="http://instagram.com/blehnert">&#62253;</a></li>
+  			<li><a href="http://facebook.com/benediktlehnert">&#62222;</a></li>
+  			<li><a href="http://de.linkedin.com/in/benediktlehnert/en">&#62233;</a></li>
+  			<li><a href="http://dribbble.com/ben.">&#62235;</a></li>-->
+			<li><a href="mailto:blehnert@me.com">&#128319;</a></li>  			
+  		</ul>
+  	</div>
 
 </section>
 
