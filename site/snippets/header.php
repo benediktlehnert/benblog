a5dc4fc18d1bb2843a9c5e8b98a2b28123a798a0
Benedikt Lehnert <benedikt@6wunderkinder.com>
2013-12-30 20:20:37 +0100

    New blog post "2013 in Tech" and new typeface

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,16 +7,18 @@
   <meta name="description" content="<?php echo html($site->description()) ?>" />
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />
-
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  	<link href="assets/favicon.ico" rel="shortcut icon">
-	<link href="assets/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="assets/favicon.ico" rel="shortcut icon">
+  <link href="assets/apple-touch-icon.png" rel="apple-touch-icon">
+  <link rel="alternate" type="application/rss+xml" href="<?php echo url('blog/feed') ?>" title="<?php echo html($pages->find('blog/feed')->title()) ?>" />
+  
+  <script type="text/javascript" src="//use.typekit.net/rhv1myx.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
-	<link rel="alternate" type="application/rss+xml" href="<?php echo url('blog/feed') ?>" title="<?php echo html($pages->find('blog/feed')->title()) ?>" />
-
-  	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,900' rel='stylesheet' type='text/css'>
+  	<!--
 	<link href='http://fonts.googleapis.com/css?family=Crimson+Text:400,400italic' rel='stylesheet' type='text/css'>
+	-->
 
   <?php echo css('assets/styles/styles.css') ?>
 
