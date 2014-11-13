9c6d17ed79e98cd091656904eb6aec023ed27dc8
Benedikt Lehnert <benedikt@6wunderkinder.com>
2014-11-13 13:55:35 +0100

    Facebook preview fix

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,20 +5,21 @@
   <title><?php echo html($site->title()) ?> - <?php echo html($page->title()) ?></title>
   <meta charset="utf-8" />
   
-  <meta name="description" content="<?php echo html($site->description()) ?>" />
+  <meta name="title" content="<?php echo html($page->title()) ?>" />
+  <!-- <meta name="description" content="<?php echo html($site->description()) ?>" /> -->
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
-  <!-- for Facebook -->          
-  <meta property="og:title" content="<?php echo html($site->title()) ?>" />
-  <meta property="og:url" content="<?php echo url() ?>" />
-  <meta property="og:type" content="website" />
+  <!-- for Facebook -->        
+  <meta property="og:title" content="<?php echo html($page->title()) ?>" />
+  <!-- <meta property="og:url" content="<?php echo url($page->url() ) ?>" /> -->
+  <meta property="og:type" content="article" />
   <meta property="og:image" content="http://www.benedikt-lehnert.de/assets/images/ben_profile.jpg" />
-  <meta property="og:description" content="">
+  <meta property="og:description" content="">  
 
   <link href="assets/favicon.ico" rel="shortcut icon">
-   <link href="favicon.ico" rel="shortcut icon">
+  <link href="favicon.ico" rel="shortcut icon">
   <link href="assets/apple-touch-icon.png" rel="apple-touch-icon">
   <link rel="alternate" type="application/rss+xml" href="<?php echo url('blog/feed') ?>" title="<?php echo html($pages->find('blog/feed')->title()) ?>" />
   
